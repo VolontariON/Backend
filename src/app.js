6bf82b0ddc603656ff3ca0e283ea6567ce8e5031
MarcoMattiuz <marccs35@gmail.com>
2024-11-23 17:47:32 +0100

    fix: express.json limit

--- src/app.js
+++ src/app.js
@@ -12,7 +12,6 @@ const swaggerDocument = JSON.parse(
 
 // setup
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(express.json());
 app.use(cookieParser());
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ limit: "16mb", extended: true }));

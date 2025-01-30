a80d774a5f3d55d00fce7fd9eaee2a37948eb058
guojie zhan <guojie.zhan@gmail.com>
2025-01-30 15:03:31 +0100

    feat:crea evento

--- src/controllers/EventsController.js
+++ src/controllers/EventsController.js
@@ -1,4 +1,5 @@
 import Eventi from "../models/EventiModel.js";
+import Associazione from "../models/AssociazioneModel.js";
 import logger from "../utils/logger.js";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
@@ -19,4 +20,30 @@ export const getEventi = async (req, res) => {
       res.status(500).json({ error: "server error" });
       logger.error(err);
     });
+};
+
+
+export const creaEvento = async (req, res) => {
+  // *SWAGGER
+  try {
+    logger.info("creaEvento: " + req.body);
+
+    const jwtuserid = req.jwtuser._id;
+    const associazione = await Associazione.findById(jwtuserid).select(
+    );
+
+    const evento = new Eventi(req.body);
+    evento.hostAssociation = jwtuserid;
+    await evento.save();
+
+    associazione.createdEvents.push(evento.id);
+
+    await associazione.save();
+
+    res.status(201).json(evento);
+    logger.info("creaEvento: " + res.statusCode);
+  } catch (err) {
+    res.status(500).json({ error: "server error" });
+    logger.error(err);
+  }
 };
--- src/routes/EventsRoutes.js
+++ src/routes/EventsRoutes.js
@@ -2,7 +2,9 @@ import express from "express";
 import { cookiejwtAuth } from "../middleware/cookiejwtAuth.js";
 import { payloadSize } from "../middleware/payloadSize.js";
 import { getEventi } from "../controllers/EventsController.js";
+import { creaEvento } from "../controllers/EventsController.js";
 const router = express.Router();
 
 router.get("/getEventi", cookiejwtAuth, getEventi);
+router.post("/creaEvento", cookiejwtAuth, creaEvento);
 export default router;

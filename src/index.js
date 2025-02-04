1b774a1a40dcf1111d5e7fd78133d423eadb85b7
MarcoMattiuz <marccs35@gmail.com>
2025-02-04 17:51:36 +0100

    deploy: modified index.js

--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ import logger from "./utils/logger.js";
 import "dotenv/config";
 const PORT = process.env.PORT || 8080;
 var server = null;
-const mongoConnect = async () => {
-  let intervalId;
-  intervalId = setInterval(() => {
-    logger.warn("Attempting to connect to MongoDB...");
-  }, 1000);
-  // await mongoose.connect(
-  //   `mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`
-  // );
-  await mongoose.connect(process.env.DATABASE_URI);
+// const mongoConnect = async () => {
+//   let intervalId;
+//   intervalId = setInterval(() => {
+//     logger.warn("Attempting to connect to MongoDB...");
+//   }, 1000);
 
-  clearInterval(intervalId);
-  logger.info(`connected to mongoDB on mongodb://${process.env.DATABASE_URI}`);
-};
+//   clearInterval(intervalId);
+//   logger.info(`connected to mongoDB on mongodb://${process.env.DATABASE_URI}`);
+// };
 
 const startServer = () => {
   server = app.listen(PORT, () => {
@@ -24,7 +20,9 @@ const startServer = () => {
   });
 };
 try {
-  await mongoConnect();
+  // await mongoConnect();
+  await mongoose.connect(process.env.DATABASE_URI);
+  logger.info(`connected to mongoDB on mongodb://${process.env.DATABASE_URI}`);
   startServer();
   mongoose.connection.on("disconnected", async () => {
     logger.error("MongoDB connection lost!");
